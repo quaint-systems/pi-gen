34e26d4c08b59bd8817bf6911aae0a89084ef2e4
CoffeeMuse <88721071+coffeemuse@users.noreply.github.com>
2025-02-22 06:53:40 -0600

    First pass

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -6,21 +6,26 @@ sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
 if [ -n "$APT_PROXY" ]; then
-	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
-	sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
+    install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
+    sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
 else
-	rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
+    rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 fi
 
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+# Add Zabbly repository and key
+install -m 644 files/zabbly-incus-lts-6.0.sources "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/zabbly.asc "${ROOTFS_DIR}/etc/apt/keyrings/zabbly.asc"
+
 on_chroot <<- \EOF
-	ARCH="$(dpkg --print-architecture)"
-	if [ "$ARCH" = "armhf" ]; then
-		dpkg --add-architecture arm64
-	elif [ "$ARCH" = "arm64" ]; then
-		dpkg --add-architecture armhf
-	fi
-	apt-get update
-	apt-get dist-upgrade -y
+    ARCH="$(dpkg --print-architecture)"
+    if [ "$ARCH" = "armhf" ]; then
+        dpkg --add-architecture arm64
+    elif [ "$ARCH" = "arm64" ]; then
+        dpkg --add-architecture armhf
+    fi
+    apt-get update
+    apt-get dist-upgrade -y
 EOF
